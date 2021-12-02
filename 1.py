c5b8d9ca335b8220f3caf45d1fbf3e55ee194d63
James Hart <james.elliot.hart@gmail.com>
2021-12-02 16:44:18 +0000

    day 1 part 2

--- 1.py
+++ 1.py
@@ -13,14 +13,38 @@ sample = [
     263,
 ]
 
+
 def get_comparer(measurements):
     return [x[1] - x[0] for x in zip(measurements, measurements[1:])]
+
 
 def increases(comparisons):
     return len([x for x in comparisons if x > 0])
 
+
+class SlidingWindow():
+    def __init__(self, iterable, window_size=3):
+        self._window_size = window_size
+        self._iterable = iterable
+        self._index = 0
+        self._stop = False
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._stop:
+            raise StopIteration
+        item = sum(
+            self._iterable[self._index:self._index + self._window_size])
+        self._index += 1
+        if len(self._iterable[self._index:]) < 3:
+            self._stop = True
+        return item
+
+
 if __name__ == '__main__':
     file_name = '1.txt'
+    data = sample
     data = [int(x) for x in shared.get_data(file_name)]
-    print(increases(get_comparer(data)))
-    # print(shared.download(f'{file_name}.txt'))
+    print(increases(get_comparer(list(SlidingWindow(data)))))
