418b0c0353c36caddfb5714adb0f47c008cab582
James Hart <james.elliot.hart@gmail.com>
2021-12-05 21:12:41 +0000

    handle messages

--- static/main.js
+++ static/main.js
@@ -1,3 +1,35 @@
+const NO_ANSWER = { 'part1': '', 'part2': '' }
+
+const hideError = () => document.getElementById('errorSection').hidden = true
+
+const updateAnswer = data => {
+    var part1 = document.getElementById('solution1');
+    var part2 = document.getElementById('solution2');
+    part1.innerText = data.part1
+    part2.innerText = data.part2
+}
+
+const showError = data => {
+    var errorSection = document.getElementById('errorSection')
+    var errorText = document.getElementById('errorText')
+    errorSection.hidden = false
+    errorText.innerText = data
+}
+
+const resetForm = () => document.getElementById('puzzleInput').value = ''
+
+function processMessage(message) {
+    var actions = {
+        'success': payload => { hideError(); updateAnswer(payload); },
+        'timeout': payload => { showError(payload); updateAnswer(NO_ANSWER); },
+        'failure': payload => { showError(payload); updateAnswer(NO_ANSWER); },
+        'reset': _ => { hideError(); updateAnswer(NO_ANSWER); resetForm() },
+        'unrecognized': category => console.error(`Unrecognized message category [${category}]!`),
+    }
+    var action = actions[message.category] || function (_) { actions.unrecognized(message.category) }
+    action(message.payload)
+}
+
 function addSubmit(ev) {
     ev.preventDefault()
     fetch('/solve', {
@@ -5,29 +37,24 @@ function addSubmit(ev) {
         body: new FormData(this)
     })
         .then(resp => resp.json())
-        .then(addShow)
+        .then(processMessage)
 }
 
-
-function addShow(data) {
-    var part1 = document.getElementById('solution1');
-    var part2 = document.getElementById('solution2');
-    part1.innerText = data['part1']
-    part2.innerText = data['part2']
-}
-
-function addClear(ev) {
+function addReset(ev) {
     ev.preventDefault()
-    var puzzleInput = document.getElementById('puzzleInput')
-    var solutions = document.getElementsByClassName('solution')
-    puzzleInput.value = ''
-    for (const e of solutions) {
-        e.innerHTML = ''
+    message = {
+        'category': 'reset',
+        'payload': '',
     }
+    processMessage(message)
 }
 
-var form = document.getElementById('solve')
-form.addEventListener('submit', addSubmit)
+function main() {
+    var form = document.getElementById('solve')
+    form.addEventListener('submit', addSubmit)
 
-var clear = document.getElementById('clear-button')
-clear.addEventListener('click', addClear)
+    var clear = document.getElementById('clear-button')
+    clear.addEventListener('click', addReset)
+}
+
+main()
