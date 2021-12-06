1630aa460703efa94cef0be4fda62a12e9a008b7
James Hart <james.elliot.hart@gmail.com>
2021-12-06 14:54:48 +0000

    standardize to part_1 and part_2 functions

--- _template.py
+++ _template.py
@@ -1,16 +1,23 @@
-import shared
 import os
+import shared
 
 sample = [
 
 ]
 
+file_name = f'{os.path.basename(__file__).split(".")[0]}.txt'
+DATA = sample
+DATA = shared.get_data(file_name)
 
-def main():
-    file_name = f'{os.path.basename(__file__).split(".")[0]}.txt'
-    data = [int(x) for x in sample]
-    data = [int(x) for x in shared.get_data(file_name)]
+
+def part_1(data):
+    return data
+
+
+def part_2(data):
+    return data
 
 
 if __name__ == '__main__':
-    main()
+    print(f'Part 1: {part_1(DATA)}')
+    print(f'Part 2: {part_2(DATA)}')
