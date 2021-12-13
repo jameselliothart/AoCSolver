95ecdb277348b70b0efc8511a79f341b008fb6d0
James Hart <james.elliot.hart@gmail.com>
2021-12-13 21:31:22 +0000

    test stopwatch.start

--- static/test.js
+++ static/test.js
@@ -1,8 +1,16 @@
 import { newStopwatch } from "./stopwatch.js";
 
-QUnit.module('stopwatch should', function() {
-    QUnit.test('set to 0 with reset', function(assert) {
-        var sw = newStopwatch(i => assert.equal(i, 0))
+QUnit.module('stopwatch should', () => {
+    QUnit.test('set to 0 with reset', assert => {
+        var sw = newStopwatch(i => assert.strictEqual(i, 0))
         sw.reset()
     })
-});
+    QUnit.test('update continuously once started', assert => {
+        const done = assert.async()
+        assert.timeout(500)
+        var counter = { count: 0 }
+        var sw = newStopwatch(i => {counter.count++; console.log(`stopwatch ticker: ${i}`)})
+        sw.start()
+        setTimeout(() => {sw.stop(); assert.ok(counter.count > 5); done();}, 100)
+    })
+})
