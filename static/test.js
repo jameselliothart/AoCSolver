94cda424041788872bcb061143868b42bcb34bcc
James Hart <james.elliot.hart@gmail.com>
2021-12-14 15:40:08 +0000

    add controller test

--- static/test.js
+++ static/test.js
@@ -1,4 +1,7 @@
 import { newStopwatch } from "./stopwatch.js";
+import { newController } from "./controller.js";
+import { noop } from "./actions.js";
+
 
 QUnit.module('stopwatch should', () => {
     QUnit.test('set to 0 with reset', assert => {
@@ -12,5 +15,17 @@ QUnit.module('stopwatch should', () => {
         var sw = newStopwatch(i => {counter.count++; console.log(`stopwatch ticker: ${i}`)})
         sw.start()
         setTimeout(() => {sw.stop(); assert.ok(counter.count > 5); done();}, 100)
+    })
+})
+
+QUnit.module('controller should', () => {
+    QUnit.test('gracefully ignore unrecognized message categories', async assert => {
+        var controller = newController({noop:noop})
+        try {
+            await controller.process({ category: 'nonsense', payload: ''})
+            assert.ok(true)
+        } catch (e) {
+            assert.ok(false, `Exception was thrown: ${e}`)
+        }
     })
 })
