dfe30949c9fd0dda23026a16f9bcd46c53fe9c58
James Hart <james.elliot.hart@gmail.com>
2021-12-14 15:51:59 +0000

    formatting

--- static/test.js
+++ static/test.js
@@ -12,26 +12,26 @@ QUnit.module('stopwatch should', () => {
         const done = assert.async()
         assert.timeout(500)
         var counter = { count: 0 }
-        var sw = newStopwatch(i => {counter.count++; console.log(`stopwatch ticker: ${i}`)})
+        var sw = newStopwatch(i => { counter.count++; console.log(`stopwatch ticker: ${i}`) })
         sw.start()
-        setTimeout(() => {sw.stop(); assert.ok(counter.count > 5); done();}, 100)
+        setTimeout(() => { sw.stop(); assert.ok(counter.count > 5); done(); }, 100)
     })
 })
 
 QUnit.module('controller should', () => {
     QUnit.test('gracefully ignore unrecognized message categories', async assert => {
-        var controller = newController({noop:noop})
+        var controller = newController({ noop: noop })
         try {
-            await controller.process({ category: 'nonsense', payload: ''})
+            await controller.process({ category: 'nonsense', payload: '' })
             assert.ok(true)
         } catch (e) {
             assert.ok(false, `Exception was thrown: ${e}`)
         }
     })
     QUnit.test('dispatch actions based on message category', async assert => {
-        var counter = {count:0}
-        var actions = {noop:noop, updateCounter: payload => {counter.count = payload; return noop;}}
-        var message = {category:'updateCounter', payload:10}
+        var counter = { count: 0 }
+        var actions = { noop: noop, updateCounter: payload => { counter.count = payload; return noop; } }
+        var message = { category: 'updateCounter', payload: 10 }
         var controller = newController(actions)
         await controller.process(message)
         assert.strictEqual(counter.count, message.payload, 'Check message processing')
