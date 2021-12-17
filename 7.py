5af94ee71a1ab62cb678a755b39b24475d99b68f
James Hart <james.elliot.hart@gmail.com>
2021-12-17 19:06:04 +0000

    day 7 part 1

---
+++ 7.py
@@ -0,0 +1,41 @@
+import os
+import file_io
+
+sample = [
+    '16,1,2,0,4,2,7,1,2,14'
+]
+
+file_name = f'{os.path.basename(__file__).split(".")[0]}.txt'
+DATA = sample
+DATA = file_io.get_data(file_name)
+
+
+def cost_to_move(starting: int, end: int):
+    return abs(starting - end)
+
+
+def get_fuel_costs(positions: list[int], destination: int):
+    return [cost_to_move(position, destination) for position in positions]
+
+
+def total_cost(fuel_costs: list[int]):
+    return sum(fuel_costs)
+
+
+def part_one(data):
+    [positions_raw, *_] = data
+    positions = [int(p) for p in positions_raw.split(',')]
+    possibilities = [
+        (destination, total_cost(get_fuel_costs(positions, destination)))
+        for destination in range(max(positions) + 1)
+    ]
+    return min(possibilities, key=lambda x: x[1])
+
+
+def part_two(data):
+    pass
+
+
+if __name__ == '__main__':
+    print(f'Part 1: {part_one(DATA)}')
+    print(f'Part 2: {part_two(DATA)}')
