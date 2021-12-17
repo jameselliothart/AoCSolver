acd0eec4315e0980c9b18839aac17a5d08e30d18
James Hart <james.elliot.hart@gmail.com>
2021-12-17 18:31:39 +0000

    cleaner design, not efficient enough for part 2

--- 6.py
+++ 6.py
@@ -11,44 +11,47 @@ file_name = f'{os.path.basename(__file__).split(".")[0]}.txt'
 DATA = sample
 DATA = file_io.get_data(file_name)
 
-@dataclass(frozen=True)
+
+@dataclass
 class Fish:
-    timer: int
-
-    @staticmethod
-    def new():
-        return Fish(8)
-
-    @staticmethod
-    def mature():
-        return Fish(6)
+    days_left: int
+    timer: int = 8
 
 
-def pass_day(fish: Fish):
-    if fish.timer > 0:
-        yield Fish(fish.timer - 1)
-    else:
-        yield Fish.new()
-        yield Fish.mature()
+def num_offspring(timer, days_left, so_far):
+    if days_left - timer <= 0:
+        return so_far
+    if timer != 6:
+        return num_offspring(6, days_left - timer - 1, 1)
+    return (days_left // (timer + 1)) + so_far
 
 
-def pass_days(fishes: list[Fish], stopping_day: int, current_day: int):
-    if current_day == stopping_day:
-        return fishes
-    new_fishes = list(chain(*(pass_day(fish) for fish in fishes)))
-    return pass_days(new_fishes, stopping_day, current_day + 1)
+def get_offspring(fish: Fish):
+    if fish.timer + 1 > fish.days_left:
+        return []
+    first_offspring_days_left = fish.days_left - fish.timer - 1
+    offspring = [Fish(days_left)
+                 for days_left in range(first_offspring_days_left, -1, -7)]
+    return offspring
+
+
+def count_lineage_of(fish: Fish):
+    offspring = get_offspring(fish)
+    return len([fish]) + sum(count_lineage_of(f) for f in offspring)
 
 
 def part_one(data):
     fish_string, *_ = data
-    fishes = [Fish(int(x)) for x in fish_string.split(',')]
-    return len(pass_days(fishes, 80, 0))
+    fishes = [Fish(80, int(x)) for x in fish_string.split(',')]
+    return sum(count_lineage_of(fish) for fish in fishes)
 
 
 def part_two(data):
-    pass
+    fish_string, *_ = data
+    fishes = [Fish(256, int(x)) for x in fish_string.split(',')]
+    return sum(count_lineage_of(fish) for fish in fishes)
 
 
 if __name__ == '__main__':
     print(f'Part 1: {part_one(DATA)}')
-    print(f'Part 2: {part_two(DATA)}')
+    # print(f'Part 2: {part_two(DATA)}')
